d1f782180f795178fccd068c52919a21bb705a6c
Sujeet2003 <sujeetsmpatel@gmail.com>
2024-12-11 17:17:26 +0530

    added files for railway deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn TMRH.wsgi
--- TMRH/wsgi.py
+++ TMRH/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TMRH.settings')
 
 application = get_wsgi_application()
 
-app = application
+# app = application
